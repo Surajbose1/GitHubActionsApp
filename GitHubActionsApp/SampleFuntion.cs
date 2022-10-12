dbc7dcf70952de6944d03156235c4b266b3b7623
Suraj Bose <suraj.bose@ghd.com>
2022-10-12 12:52:05 +1100

    Add project files.

---
+++ GitHubActionsApp/SampleFuntion.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GitHubActionsApp
+{
+    public class SampleFuntion
+    {
+        [FunctionName(nameof(SampleFuntion))]
+        public async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = string.IsNullOrEmpty(req.Query["name"])  ? "World" : req.Query["name"];
+            string responseMessage = $"Hello {name}";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
